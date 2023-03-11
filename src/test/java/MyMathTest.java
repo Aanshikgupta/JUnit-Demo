b71bc433f67d9cdfc2b0af98d13e7c6a23955c7a
Aanshik Gupta <aanshik.gupta.in@freecharge.com>
2023-03-11 13:06:50 +0530

    JUnit Demo Completed

--- src/test/java/MyMathTest.java
+++ src/test/java/MyMathTest.java
@@ -2,13 +2,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
+//Test Class inside test folder
 public class MyMathTest {
 
     List<String> todos= Arrays.asList("AWS", "Amazon", "GCP", "Others");
 
+    //Annotation important for test
     @Test
     void calculateSum_FourMemberArray(){
 //        fail("Test not implemented yet!");
@@ -16,9 +17,12 @@ public class MyMathTest {
         int res=myMath.getSum(new int[]{1,2,3,4});
         System.out.println(res);
         int expected=10;
+
+        //check if the condition satisfies
         assertEquals(expected,res);
     }
 
+    //many test for same case
     @Test
     void calculateSum_ZeroMemberArray(){
         MyMath myMath=new MyMath();
@@ -31,5 +35,6 @@ public class MyMathTest {
     @Test
     void todosTest(){
         assertEquals(true,todos.contains("AWS"));
+        assertTrue(todos.contains("AWS"), "Error Message if false");
     }
 }
