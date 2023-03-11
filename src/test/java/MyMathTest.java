91e472b13d969242cb3c1580dfd1e4a2011d8550
Aanshik Gupta <aanshik.gupta.in@freecharge.com>
2023-03-11 12:28:27 +0530

    New Test Created

--- src/test/java/MyMathTest.java
+++ src/test/java/MyMathTest.java
@@ -16,4 +16,15 @@ public class MyMathTest {
 
         assertEquals(expected,res);
     }
+
+    @Test
+    void Test1(){
+        MyMath myMath=new MyMath();
+        int res=myMath.getSum(new int[]{});
+        System.out.println(res);
+
+        int expected=0;
+
+        assertEquals(expected,res);
+    }
 }
