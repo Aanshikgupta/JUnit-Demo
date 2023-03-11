d6f45da81ca29faa7c9fbbe31b075b35579a9180
Aanshik Gupta <aanshik.gupta.in@freecharge.com>
2023-03-11 13:02:15 +0530

    JUnit Demo Completed

--- src/test/java/MyMathTest.java
+++ src/test/java/MyMathTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MyMathTest {
+
+    List<String> todos= Arrays.asList("AWS", "Amazon", "GCP", "Others");
 
     @Test
     void calculateSum_FourMemberArray(){
@@ -22,5 +26,10 @@ public class MyMathTest {
         System.out.println(res);
         int expected=0;
         assertEquals(expected,res);
+    }
+
+    @Test
+    void todosTest(){
+        assertEquals(true,todos.contains("AWS"));
     }
 }
