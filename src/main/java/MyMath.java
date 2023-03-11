e6a96465ed3869b2bf134c393719470fb932c168
Aanshik Gupta <aanshik.gupta.in@freecharge.com>
2023-03-11 12:26:20 +0530

    First Test

---
+++ src/main/java/MyMath.java
@@ -0,0 +1,9 @@
+public class MyMath {
+    public int getSum(int[] arr){
+        int sum=0;
+        for(Integer x:arr){
+            sum+=x;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/MyMathTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MyMathTest {
+
+    @Test
+    void MathTest(){
+//        fail("Test not implemented yet!");
+        MyMath myMath=new MyMath();
+        int res=myMath.getSum(new int[]{1,2,3,4});
+        System.out.println(res);
+
+        int expected=10;
+
+        assertEquals(expected,res);
+    }
+}
